4d3f798275e36e5c838bba52a71c7f296aebc4d6
Coenagrion <maria7873878@gmail.com>
2023-03-11 20:27:25 +0300

    Исправлена ошибка с повторным добавлением категорий при повторном входе.

--- bookkeeper/main.py
+++ bookkeeper/main.py
@@ -4,19 +4,33 @@ from bookkeeper.presenter.expense_presenter import ExpensePresenter
 from bookkeeper.models.category import Category
 from bookkeeper.models.expense import Expense
 from bookkeeper.repository.sqlite_repository import SQLiteRepository
+from bookkeeper.utils import read_tree
 import sys
+
+cats = '''
+продукты
+    мясо
+        сырое мясо
+        мясные продукты
+    сладости
+книги
+одежда
+'''.splitlines()
 
 DB_NAME = 'test.db'
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
 
-    view = MainWindow()
-    model = None  # TODO: здесь должна быть модель
-
     cat_repo = SQLiteRepository[Category](DB_NAME, Category)
     exp_repo = SQLiteRepository[Expense](DB_NAME, Expense)
 
-    window = ExpensePresenter(model, view, cat_repo, exp_repo)  # TODO: передать три репозитория
+    if not cat_repo.get_all():
+        Category.create_from_tree(read_tree(cats), cat_repo)
+
+    view = MainWindow()
+    model = None
+
+    window = ExpensePresenter(model, view, cat_repo, exp_repo)
     window.show()
     app.exec()
