5aa0f4d2cb4739ef77c19246093a36e7e824b554
Coenagrion <maria7873878@gmail.com>
2023-03-11 17:37:38 +0300

    Добавлена возможность удаления записи пользователем

--- bookkeeper/presenter/expense_presenter.py
+++ bookkeeper/presenter/expense_presenter.py
@@ -7,6 +7,7 @@ class ExpensePresenter:
         self.exp_data = None
         self.cat_data = cat_repo.get_all()  # TODO: implement update_cat_data() similar to update_expense_data()
         self.view.on_expense_add_button_clicked(self.handle_expense_add_button_clicked)
+        self.view.on_expense_delete_button_clicked(self.handle_expense_delete_button_clicked)
         self.view.on_category_edit_button_clicked(self.handle_category_edit_button_clicked)
 
     def update_expense_data(self):
@@ -27,5 +28,11 @@ class ExpensePresenter:
         exp = Expense(int(amount), category=cat_pk)
         self.exp_repo.add(exp)
         self.update_expense_data()
+    def handle_expense_delete_button_clicked(self) -> None:
+        selected = self.view.get_selected_expenses()
+        if selected:
+            for e in selected:
+                self.exp_repo.delete(e)
+            self.update_expense_data()
     def handle_category_edit_button_clicked(self):
         self.view.show_cats_dialog(self.cat_data)
--- bookkeeper/view/expense_view.py
+++ bookkeeper/view/expense_view.py
@@ -69,6 +69,9 @@ class MainWindow(QtWidgets.QMainWindow):
         self.expense_add_button = QPushButton('Добавить')
         self.bottom_controls.addWidget(self.expense_add_button, 2, 1)
 
+        self.expense_delete_button = QPushButton('Удалить')
+        self.bottom_controls.addWidget(self.expense_delete_button, 2, 2)
+
         self.bottom_widget = QWidget()
         self.bottom_widget.setLayout(self.bottom_controls)
 
@@ -94,6 +97,9 @@ class MainWindow(QtWidgets.QMainWindow):
     def on_expense_add_button_clicked(self, slot):
         self.expense_add_button.clicked.connect(slot)
 
+    def on_expense_delete_button_clicked(self, slot):
+        self.expense_delete_button.clicked.connect(slot)
+
     def get_amount(self) -> float:
         if self.amount_line_edit.text() == "":
             return 0
@@ -101,6 +107,15 @@ class MainWindow(QtWidgets.QMainWindow):
             return float(self.amount_line_edit.text())
         else:
             raise Exception
+
+    def __get_selected_row_indices(self) -> list[int]:
+        return list(set([qmi.row() for qmi in self.expenses_grid.selectionModel().selection().indexes()]))
+
+    def get_selected_expenses(self) -> list[int] | None:
+        idx = self.__get_selected_row_indices()
+        if not idx:
+            return None
+        return [self.item_model._data[i].pk for i in idx]
 
     def get_selected_cat(self) -> int:
         return self.category_dropdown.itemData(self.category_dropdown.currentIndex())
